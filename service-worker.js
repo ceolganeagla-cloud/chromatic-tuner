41ae2b540de361569863bd957f061962c57cc568
ceolganeagla-cloud <ceolganeagla@gmail.com>
2025-10-10 19:23:16 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// Minimal service worker so the app can be installed on phones.
-// Extend with caching later if you want offline support.
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('activate', (e) => self.clients.claim());
