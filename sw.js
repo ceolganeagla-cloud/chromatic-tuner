47e24bcf0fffc03caea74dfb7a16300c5b1516cb
ceolganeagla-cloud <ceolganeagla@gmail.com>
2025-10-10 19:29:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,49 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("cge-tuner-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icons/icon-192.png",
-        "./icons/icon-512.png"
-      ]);
-    })
+const CACHE = "cge-tuner-v2"; // bump this when you change files
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/maskable-192.png",
+  "./icons/maskable-512.png",
+  "./icons/apple-touch-icon.png",
+  "./logo.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+
+  // Always try the network for navigations (so index.html updates fast).
+  if (req.mode === "navigate") {
+    e.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put("./", copy)).catch(()=>{});
+        return res;
+      }).catch(() => caches.match("./"))
+    );
+    return;
+  }
+
+  // Cache-first for static assets
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+      return res;
+    }))
   );
 });
